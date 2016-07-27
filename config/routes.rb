a0cb170aac29d74b98d2ba3ca9e8fcc98e5eb208
Matt Sears <msears02@gmail.com>
2016-07-26 21:02:22 -0600

    added default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'greetings/hello'
 
+   # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+   root :to => 'greetings#index.html
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
