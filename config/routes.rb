02c78663576a3e9641f4fc05c6441ee049992f89
Matthew <msears02@gamil.com>
2016-12-18 16:22:57 -0700

    updated routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'greetings/hello'
 
+  root 'greetings/hello'
+
    # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'greetings/hello'
